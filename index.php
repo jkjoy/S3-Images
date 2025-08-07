4d6fb8302722bf538e4dfd4ca74a7610fa3e5ac6
浪子 <jkjoy@live.cn>
2025-08-07 19:16:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ $imageFiles = getImagesFromS3($s3Client, $bucketName, $prefix, $imageExtensions)
                 <h1><a href="/"><strong>HOME</strong></a></h1>
                 <nav>
                     <ul>
-                        <li><a href="#footer" class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="12" height="12" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z"></path></svg>About</a></li>
+                        <li><a href="#footer" class="icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="16" height="16" fill="currentColor"><path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z"></path></svg>About</a></li>
                     </ul>
                 </nav>
             </header>
